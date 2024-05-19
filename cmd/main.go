4fbf3612755804865f8a41ffec21ce4b9d00d5e8
Hanagasumiiii <veritsov@mail.ru>
2024-05-19 13:50:29 +0300

    structure without database

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"game-server-api/internal/config"
+	"game-server-api/internal/database"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
+func main() {
+	path, err := filepath.Abs("config.yml")
+	if err != nil {
+		log.Fatalf("Error getting absolute path: %s", err)
+	}
+
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		log.Fatalf("Config file not found at path: %s", path)
+	} else if err != nil {
+		log.Fatalf("Error accessing config file: %s", err)
+	}
+
+	log.Printf("Loading configuration from path: %s", path)
+
+	cfg, err := config.LoadConfig("config.yml")
+	if err != nil {
+		log.Fatalf("Failed to load config file: %s", err)
+	}
+
+	db := database.NewConnection(*cfg)
+
+	//userService := user.NewService(db)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Server is running!")
+	})
+	http.HandleFunc("/testdb", func(w http.ResponseWriter, r *http.Request) {
+		if err := db.Ping(); err != nil {
+			http.Error(w, "DB connection failed: "+err.Error(), http.StatusInternalServerError)
+		} else {
+			fmt.Fprintln(w, "DB connection successful!")
+		}
+	})
+
+	log.Println("Starting server on port", cfg.Postgres.Port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", cfg.Postgres.Port), nil); err != nil {
+		log.Fatal("Server failed:", err)
+	}
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -2,10 +2,14 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
+	"game-server-api/internal/config"
 	_ "github.com/lib/pq"
 )
 
-func NewConnection(connectionString string) *sql.DB {
+func NewConnection(cfg config.Config) *sql.DB {
+	connectionString := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable", cfg.Postgres.Host, cfg.Postgres.Database, cfg.Postgres.User, cfg.Postgres.Password)
+
 	db, err := sql.Open("postgres", connectionString)
 	if err != nil {
 		panic("failed to connect to database: " + err.Error())
